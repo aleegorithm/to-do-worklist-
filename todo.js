a5cc2813497267439c626837f91f764743a4093c
Alester Santos <santos.alester.a@gmail.com>
2024-02-14 21:28:18 +0800

    DOne

---
+++ todo.js
@@ -0,0 +1,40 @@
+const taskInput = document.getElementById("taskInput");
+const tasks = document.getElementById("tasks");
+
+function addTask(){
+    if(taskInput.value === ""){
+        alert("Please enter a task!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = taskInput.value;
+        tasks.appendChild(li); 
+        let span = document.createElement("span")
+        span.innerHTML =  "\u00d7";
+        li.appendChild(span);
+    }
+
+    taskInput.value = "";
+    saveData();
+}
+
+
+tasks.addEventListener( "click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } 
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+function saveData(){
+    localStorage.setItem("data", tasks.innerHTML);
+}
+
+function showTask(){
+    tasks.innerHTML = localStorage.getItem("data");
+}
+showTask();
